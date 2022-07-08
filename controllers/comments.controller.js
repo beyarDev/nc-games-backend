367a554c296d8a62103e4925b1d768790b77be6a
beyarDev <beyarDev@gmail.com>
2022-07-08 11:13:09 +0100

    add patch comment votes functionallity

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,9 +1,24 @@
-const { deleteCommentById } = require("../models/comments.models");
+const {
+  deleteCommentById,
+  updateCommentVoteById,
+} = require("../models/comments.models");
+
 exports.removeCommentById = async (req, res, next) => {
   const { comment_id } = req.params;
   try {
     await deleteCommentById(comment_id);
     res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.patchCommentVoteById = async (req, res, next) => {
+  const { comment_id } = req.params;
+  const { inct_votes } = req.body;
+  try {
+    const comment = await updateCommentVoteById(comment_id, inct_votes);
+    res.status(200).send({ comment });
   } catch (err) {
     next(err);
   }
--- models/comments.models.js
+++ models/comments.models.js
@@ -12,3 +12,24 @@ exports.deleteCommentById = async (commentId) => {
   const queryStr = "DELETE FROM comments WHERE comment_id = $1";
   await db.query(queryStr, [commentId]);
 };
+
+exports.updateCommentVoteById = async (incVotes, commentId) => {
+  if (isNaN(Number(commentId))) {
+    return Promise.reject({
+      status: 400,
+      msg: `invalid comment ID (${commentId})`,
+    });
+  }
+  if (incVotes === undefined || isNaN(Number(incVotes))) {
+    return Promise.reject({
+      status: 400,
+      msg: `please input valid inc_votes value`,
+    });
+  }
+  await checkExist("comments", "comment_id", commentId);
+  const { rows } = await db.query(
+    "UPDATE comments SET votes = votes + $1  WHERE comment_id = $2 RETURNING *",
+    [incVotes, commentId]
+  );
+  return rows[0];
+};
